e4ceab369eaa6c780fc0102f41d5c438ce04caa6
Schwarzwald <ysjvillmark@gmail.com>
2023-04-09 15:11:31 +0800

    🚨v0.1.5

--- nonebot_plugin_eventmonitor/__init__.py
+++ nonebot_plugin_eventmonitor/__init__.py
@@ -1,7 +1,6 @@
+import nonebot
 from nonebot.rule import Rule
-from nonebot.typing import T_State
 from nonebot.plugin import on_notice
-
 from nonebot.adapters.onebot.v11 import (
     Bot, Event, Message,
     PokeNotifyEvent,
@@ -13,45 +12,39 @@ from nonebot.adapters.onebot.v11 import (
     LuckyKingNotifyEvent
 )
 
-from .stamp import *
-from .honour import *
-from .admin import *
+from .stamp import chuo_send_msg
+from .honour import monitor_rongyu
+from .admin import del_user_bye, add_user_wecome, admin_changer
+from .utils import *
 
 
 # 获取戳一戳状态
-async def _is_poke(event: Event, state: T_State) -> bool:
+async def _is_poke(event: Event) -> bool:
     return isinstance(event, PokeNotifyEvent) and event.is_tome()
 
-
 # 获取群荣誉变更
-async def _is_rongyu(bot: Bot, event: Event, state: T_State) -> bool:
+async def _is_rongyu(event: Event) -> bool:
     return isinstance(event, HonorNotifyEvent)
 
-
 # 获取文件上传
-async def _is_checker(bot: Bot, event: Event, state: T_State) -> bool:
+async def _is_checker(event: Event) -> bool:
     return isinstance(event, GroupUploadNoticeEvent)
 
-
 # 群成员减少
-async def _is_del_user(bot: Bot, event: Event, state: T_State) -> bool:
+async def _is_del_user(event: Event) -> bool:
     return isinstance(event, GroupDecreaseNoticeEvent)
 
-
 # 群成员增加
-async def _is_add_user(bot: Bot, event: Event, state: T_State) -> bool:
+async def _is_add_user(event: Event) -> bool:
     return isinstance(event, GroupIncreaseNoticeEvent)
 
-
 # 管理员变动
-async def _is_admin_change(bot: Bot, event: Event, state: T_State) -> bool:
+async def _is_admin_change(event: Event) -> bool:
     return isinstance(event, GroupAdminNoticeEvent)
 
-
 # 红包运气王
-async def _is_red_packet(bot: Bot, event: Event, state: T_State) -> bool:
+async def _is_red_packet(event: Event) -> bool:
     return isinstance(event, LuckyKingNotifyEvent)
-
 
 # 戳一戳
 chuo = on_notice(Rule(_is_poke), priority=50, block=True)
@@ -69,60 +62,58 @@ group_admin = on_notice(Rule(_is_admin_change), priority=50, block=True)
 red_packet = on_notice(Rule(_is_red_packet), priority=50, block=True)
 
 
+
 @chuo.handle()
-async def send_chuo(bot: Bot, event: Event, state: T_State):
-    if bot_name == "寄":
-        await chuo.finish("请先配置bot_name")
-    uid = event.get_user_id()                                         # 获取用户id
+async def send_chuo(event: Event):
+    uid = event.get_user_id()                                                       # 获取用户id
     try:
-        cd = event.time - chuo_CD_dir[uid]                             # 计算cd
+        cd = event.time - chuo_CD_dir[uid]                                          # 计算cd
     except KeyError:
-        cd = chuo_cd + 1                                        # 没有记录则cd为cd_time+1
+        cd = chuo_cd + 1                                                            # 没有记录则cd为cd_time+1
     if (
         cd > chuo_cd
         or event.get_user_id() in nonebot.get_driver().config.superusers
-    ):                                                                     # 记录cd
+    ):                                                                                   # 记录cd
         chuo_CD_dir.update({uid: event.time})
-    rely_msg = chuo_send_msg()
+    rely_msg = await chuo_send_msg()
     await chuo.finish(message=Message(rely_msg))
 
 
-@qrongyu.handle()
-async def send_rongyu(bot: Bot, event: HonorNotifyEvent, state: T_State):
-    if bot_qq == "寄":
-        await qrongyu.finish("请先配置bot_qq")
-    if super_qq == "寄":
-        await qrongyu.finish("请先配置SUPERUSERS")
-    rely_msg = monitor_rongyu(event.honor_type, event.user_id)
+@qrongyu.handle()                                                                       #群荣誉变化
+async def send_rongyu(event: HonorNotifyEvent, bot: Bot):
+    bot_qq = int(bot.self_id)
+    rely_msg = await monitor_rongyu(event.honor_type, event.user_id, bot_qq)
     await qrongyu.finish(message=Message(rely_msg))
 
 
-@files.handle()
-async def handle_first_receive(bot: Bot, event: GroupUploadNoticeEvent, state: T_State):
+@files.handle()                                                                         #上传群文件
+async def handle_first_receive(event: GroupUploadNoticeEvent):
     rely = f'[CQ:at,qq={event.user_id}]\n' \
            f'[CQ:image,file=https://q4.qlogo.cn/headimg_dl?dst_uin={event.user_id}&spec=640]' \
            f'\n 上传了新文件，感谢你一直为群里做贡献喵~[CQ:face,id=175]'
     await files.finish(message=Message(rely))
 
 
-@del_user.handle()
-async def user_bye(bot: Bot, event: GroupDecreaseNoticeEvent, state: T_State):
-    rely_msg = del_user_bye(event.time, event.group_id, event.user_id)
+@del_user.handle()                                                                      #退群事件
+async def user_bye(event: GroupDecreaseNoticeEvent):
+    rely_msg = await  del_user_bye(event.time, event.user_id)
     await del_user.finish(message=Message(rely_msg))
 
 
-@add_user.handle()
-async def user_welcome(bot: Bot, event: GroupIncreaseNoticeEvent, state: T_State):
-    rely_msg = add_user_wecome(event.time, event.group_id, event.user_id)
+@add_user.handle()                                                                      #入群事件
+async def user_welcome(event: GroupIncreaseNoticeEvent, bot: Bot):
+    bot_qq = int(bot.self_id)
+    rely_msg = await  add_user_wecome(event.time, event.user_id, bot_qq)
     await add_user.finish(message=Message(rely_msg))
 
 
-@group_admin.handle()
-async def admin_chance(bot: Bot, event: GroupAdminNoticeEvent, state: T_State):
-    rely_msg = admin_changer(event.sub_type, event.user_id)
+@group_admin.handle()                                                                   #管理员变动
+async def admin_chance(event: GroupAdminNoticeEvent, bot: Bot):
+    bot_qq = int(bot.self_id)
+    rely_msg = await admin_changer(event.sub_type, event.user_id, bot_qq)
     await group_admin.finish(message=Message(rely_msg))
 
-@red_packet.handle()
-async def hongbao(bot: Bot, event: LuckyKingNotifyEvent, state: T_State):
+@red_packet.handle()                                                                    #红包运气王
+async def hongbao(event: LuckyKingNotifyEvent):
     rely_msg = f"[CQ:at,qq={event.user_id}]\n本次红包运气王为：[CQ:at,qq={event.target_id}]"
     await red_packet.finish(message=Message(rely_msg))
--- nonebot_plugin_eventmonitor/honour.py
+++ nonebot_plugin_eventmonitor/honour.py
@@ -1,42 +1,29 @@
-import nonebot
-from nonebot.permission import SUPERUSER
+from .utils import superusers
 
-try:
-    bot_qq = nonebot.get_driver().config.bot_qq
-except:
-    bot_qq = "寄"
-
-try:
-    super_qq = nonebot.get_driver().config.superusers
-except:
-    super_qq = "寄"
-
-
-
-def monitor_rongyu(honor_type, user_id):
+async def monitor_rongyu(honor_type, user_id, bot_qq):  
     rely = ""
 
-    if honor_type == "talkative":
+    if honor_type == "emotion":
         if user_id == bot_qq:
-            rely = f"你们又不行了，本喵喜提龙王🐲~"
-        elif user_id == super_qq:
-            rely = f"[CQ:at,qq={user_id}]恭喜主人荣获龙王🐲标识喵~"
+            pass
+        elif user_id != superusers:
+            rely = f"[CQ:at,qq={user_id}]恭喜主人荣获快乐源泉🤣标识喵~"
         else:
-            rely = f"恭喜[CQ:at,qq={user_id}]荣获龙王🐲标识喵~"
-
+            rely = f"恭喜[CQ:at,qq={user_id}]荣获快乐源泉🤣标识喵~"
     elif honor_type == "performer":
         if user_id == bot_qq:
             pass
-        elif user_id == super_qq:
+        elif user_id != superusers:
             rely = f"[CQ:at,qq={user_id}]恭喜主人荣获群聊之火🔥标识喵~"
         else:
             rely = f"恭喜[CQ:at,qq={user_id}]荣获群聊之火🔥标识喵~"
 
-    elif honor_type == "emotion":
+    elif honor_type == "talkative":
         if user_id == bot_qq:
-            pass
-        elif user_id == super_qq:
-            rely = f"[CQ:at,qq={user_id}]恭喜主人荣获快乐源泉🤣标识喵~"
+            rely = "你们又不行了，本喵喜提龙王🐲~"
+        elif user_id != superusers:
+            rely = f"[CQ:at,qq={user_id}]恭喜主人荣获龙王🐲标识喵~"
         else:
-            rely = f"恭喜[CQ:at,qq={user_id}]荣获快乐源泉🤣标识喵~"
+            rely = f"恭喜[CQ:at,qq={user_id}]荣获龙王🐲标识喵~"
+
     return rely
