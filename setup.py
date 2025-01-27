806cc0089d9965bb208e7f1bd32002e980d91a89
Reversedeer <ysjvillmark@gmail.com>
2025-01-27 23:29:54 +0800

    🚨v0.4.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools 
+import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="nonebot_plugin_eventmonitor",
-    version="0.3.2",
+    version="0.4.0",
     author="Reversedeer",
     description="监控群事件的插件，支持戳一戳，成员变动，群荣誉，运气王变化等提示",
     long_description=long_description,
@@ -16,20 +16,19 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
     python_requires=">=3.9",
-    install_requires = [
-        'nonebot2>=2.0.0rc2',
-        'nonebot-adapter-onebot>=2.1.3',
-        'requests>=2.28.2',
-        'httpx>=0.23.1'
-        'poetry>=1.5.0'
-        'pillow>=9.4.0'
-        'sqlalchemy>=2.0.19'
-        'nonebot-plugin-apscheduler>=0.1.3'
-
-    ]
+    install_requires=[
+        "nonebot2>=2.0.0rc2",
+        "nonebot-adapter-onebot>=2.1.3",
+        "requests>=2.28.2",
+        "httpx>=0.27.2"
+        "poetry>=1.5.0"
+        "pillow>=9.4.0"
+        "sqlalchemy>=2.0.19"
+        "nonebot-plugin-apscheduler>=0.1.3",
+    ],
 )
